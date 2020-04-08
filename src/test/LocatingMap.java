835df172282c4bfc5513580b18013c7309a73e55
dreaguti <drea.guti@gmail.com>
2020-04-08 13:34:16 -0400

    Update LocatingMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -1,6 +1,9 @@
 package test;
 
 public class LocatingMap {
-
+    public static void main(String[] args){
+        System.out.println("Locating map..");
+        
+        //from GitHub
     //locating map
 }
